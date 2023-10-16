1052bd240130c9933dad663d6f3d7cec973345e0
Maxance Ferran <maxance.ferran@ynov.com>
2023-10-16 10:38:09 +0200

    CalculatorQuest

---
+++ CalculatorQuest/CalculatorQuest/Calc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Avalonia.Controls;
+using System.Linq;
+
+namespace CalculatorQuest
+{
+    public class Calc 
+    {
+        private string[] _signs = { "+", "-", "x", "/", "%" };
+
+        public Calc() {}
+
+        public string Operator(string Input)
+        {
+            string[] parts = Input.Split(_signs, StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length != 2)
+            {
+                return "Not valid operator";
+            }
+
+            double chiffre1, chiffre2;
+            if (!double.TryParse(parts[0], out chiffre1) || !double.TryParse(parts[1], out chiffre2))
+            {
+                return "Not valid Number";
+            }
+
+            if (Input.Contains("+"))
+            {
+                return (chiffre1 + chiffre2).ToString();
+            }
+            else if (Input.Contains("-"))
+            {
+                return (chiffre1 - chiffre2).ToString();
+            }
+            else if (Input.Contains("x"))
+            {
+                return (chiffre1 * chiffre2).ToString();
+            }
+            else if (Input.Contains("/"))
+            {
+                if (chiffre1 == 0 || chiffre2 == 0)
+                {
+                    return "You cannot Divide by 0 my man";
+                }
+                return (chiffre1 / chiffre2).ToString();
+            }
+            else if (Input.Contains("%"))
+            {
+                return (chiffre1 % chiffre2).ToString();
+            }
+
+            return "Operation not support";
+        }
+    }
+    
+}
+
+
---
+++ CalculatorQuest/CalculatorQuest/CalculatorScreen.axaml.cs
@@ -0,0 +1,110 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System;
+using Avalonia.Interactivity;
+
+namespace CalculatorQuest;
+
+public partial class CalculatorScreen : Window
+{
+    public string CurrentInput = "";
+    public string FirstInput = "";
+    public string Operand = "";
+
+    
+    public CalculatorScreen()
+    {
+        this.Width = 300;
+        this.Height = 600;
+        InitializeComponent();
+    }
+    
+
+    private void Number_OnClick(object? sender, RoutedEventArgs e)
+    {
+     Button? btn = (Button)sender!;
+     CurrentInput += btn.Content?.ToString();
+     NumbersBox.Content = CurrentInput;
+    }
+    
+    private void Operator_OnClick(object? sender, RoutedEventArgs e)
+    {
+        Button? btn = (Button)sender!;
+        FirstInput = CurrentInput;
+        SavedNumber.Content = FirstInput;
+        CurrentInput = "";
+        NumbersBox.Content = CurrentInput;
+
+        if (btn == More)
+        {
+            OperatorBox.Content = "+";
+        }
+        else if (btn == Less)
+        {
+            OperatorBox.Content = "-";
+        }
+        else if (btn == Multiply)
+        {
+            OperatorBox.Content = "x";
+        }
+        else if (btn == Divide)
+        {
+            OperatorBox.Content = "/";
+        }
+        else if (btn == Mod)
+        {
+            OperatorBox.Content = "%";
+        }
+        else if (btn == Sign)
+        {
+            OperatorBox.Content = "+/-";
+        }
+    }
+
+    private void C_Button_Click(object sender, RoutedEventArgs e)
+    {
+        CurrentInput = "";
+        SavedNumber.Content = "";
+        NumbersBox.Content = CurrentInput;
+    }
+    private void EqualButton_Click(object sender, RoutedEventArgs e)
+    {
+        Calc c = new Calc();
+        string operatorContent = OperatorBox.Content.ToString(); 
+
+        if (!string.IsNullOrEmpty(operatorContent))
+        {
+    
+            string expression = FirstInput + operatorContent + CurrentInput;
+
+           
+            string result = c.Operator(expression);
+            
+            SavedNumber.Content = result.ToString();
+            CurrentInput = result.ToString();
+            NumbersBox.Content = "";
+            OperatorBox.Content = "";
+        }
+    }
+    
+    private void Control_OnClick(object? sender, RoutedEventArgs e)
+    {
+        
+        Button? btn = (Button)sender!;
+        CurrentInput += btn.Content?.ToString();
+        if (btn  == ClearAll)
+        {
+            CurrentInput = "";
+            NumbersBox.Content = CurrentInput;
+        }
+        else if (btn == Del)
+        {
+            var size = CurrentInput.Length;
+            CurrentInput = CurrentInput.Remove(size - 2);
+            NumbersBox.Content = CurrentInput;
+
+        }
+       
+    }
+}
